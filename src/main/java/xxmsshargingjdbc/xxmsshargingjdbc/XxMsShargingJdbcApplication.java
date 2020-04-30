bb4d09dfdec56173bd3dd33fd72ee0d498880f3a
shaozhixing <shaozhixing@shouqiev.com>
2020-04-30 12:52:56 +0800

    123

---
+++ src/main/java/xxmsshargingjdbc/xxmsshargingjdbc/XxMsShargingJdbcApplication.java
@@ -0,0 +1,13 @@
+package xxmsshargingjdbc.xxmsshargingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxMsShargingJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxMsShargingJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xxmsshargingjdbc/xxmsshargingjdbc/XxMsShargingJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package xxmsshargingjdbc.xxmsshargingjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxMsShargingJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
